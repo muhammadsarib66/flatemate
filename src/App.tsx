d86728d4fa1d4eb2c8dfe04282f731b21224d0a4
muhammadsarib66 <saribnoor0310@gmail.com>
2024-05-21 00:19:14 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Login />} />
           <Route path="about" element={<About />} />
 
         </Routes>
